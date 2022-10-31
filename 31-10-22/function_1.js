0d2eec10520f601f7257b608bfde78e37ee39b7d
ardiansyah835 <ardianananda70@gmail.com>
2022-10-31 13:20:48 +0700

    tugas_31_10_22

---
+++ 31-10-22/function_1.js
@@ -0,0 +1,4 @@
+function myFunction(){
+    alert("This if function")
+}
+myFunction();
---
+++ remedial/remedial_ardiansyah.js
@@ -0,0 +1,23 @@
+// if else
+var angka = 17;
+if (angka%2==0)
+console.log("Genap")
+else
+console.log("Ganjil")
+
+// while
+var i=0;
+while (i<10)
+{
+    console.log("Benar")
+    i++;
+}
+
+// do while
+var i=1;
+do
+{
+    console.log("Benar")
+    i++;
+}
+while (i<=10)
